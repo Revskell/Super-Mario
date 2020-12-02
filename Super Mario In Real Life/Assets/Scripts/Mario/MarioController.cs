42d417e0a31069322f4ca2733df8150574e8feaa
Revskell <revskell04@gmail.com>
2020-12-02 15:54:21 +0100

    Mucha mierda

---
+++ Super Mario In Real Life/Assets/Scripts/Mario/MarioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioController : MonoBehaviour
+{
+
+    private float speed;
+    private bool onGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
